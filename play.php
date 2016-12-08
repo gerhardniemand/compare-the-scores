28bb41506b7610e86ab6c6267edbbcbf830ed178
Sarel van der Walt <sarel.w@gmail.com>
2016-12-08 14:13:47 +0200

    Inport class to make it more readable

--- play.php
+++ play.php
@@ -1,4 +1,6 @@
 <?php
+use Symfony\Component\VarDumper\VarDumper;
+
 include("vendor/autoload.php");
 
 // Gets the two lines from standard input (is there a better way of doing this?):
@@ -6,5 +8,5 @@ $handle = fopen("php://stdin", "r");
 fscanf($handle,"%d %d %d %d",$a0,$a1,$a2,$a3);
 fscanf($handle,"%d %d %d %d",$b0,$b1,$b2,$b3);
 
-\Symfony\Component\VarDumper\VarDumper::dump(array($a0, $a1, $a2, $a3));
-\Symfony\Component\VarDumper\VarDumper::dump(array($b0, $b1, $b2, $b3));
+VarDumper::dump(array($a0, $a1, $a2, $a3));
+VarDumper::dump(array($b0, $b1, $b2, $b3));
