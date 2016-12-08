e6d0ec486ff57cd0128f332947c8404e19682467
Sarel van der Walt <sarel.w@gmail.com>
2016-12-08 14:12:23 +0200

    Bootstrap of play script

--- play.php
+++ play.php
@@ -1,3 +1,10 @@
 <?php
 include("vendor/autoload.php");
 
+// Gets the two lines from standard input (is there a better way of doing this?):
+$handle = fopen("php://stdin", "r");
+fscanf($handle,"%d %d %d %d",$a0,$a1,$a2,$a3);
+fscanf($handle,"%d %d %d %d",$b0,$b1,$b2,$b3);
+
+\Symfony\Component\VarDumper\VarDumper::dump(array($a0, $a1, $a2, $a3));
+\Symfony\Component\VarDumper\VarDumper::dump(array($b0, $b1, $b2, $b3));
