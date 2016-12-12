c36747165b48c77ea2cdb5e8c03017c97e9058c1
gerhardniemand <g.b.niemand@gmail.com>
2016-12-12 05:18:10 +0200

    Added comparison functionality.

--- play.php
+++ play.php
@@ -9,7 +9,8 @@ $handle = fopen("php://stdin", "r");
 fscanf($handle,"%d %d %d %d",$a0,$a1,$a2,$a3);
 fscanf($handle,"%d %d %d %d",$b0,$b1,$b2,$b3);
 
-VarDumper::dump(array($a0, $a1, $a2, $a3));
-VarDumper::dump(array($b0, $b1, $b2, $b3));
+//VarDumper::dump(array($a0, $a1, $a2, $a3));
+//VarDumper::dump(array($b0, $b1, $b2, $b3));
 
 $comparer = new CompareTheScores();
+$comparer->Compare(array($a0,$a1,$a2,$a3),array($b0,$b1,$b2,$b3));
--- src/CompareTheScores.php
+++ src/CompareTheScores.php
@@ -3,8 +3,49 @@ namespace Fun;
 
 class CompareTheScores
 {
-    public function __construct()
+   public function __construct()
+   {
+     //   echo 'Yey! The constructor works...' . PHP_EOL;
+   }
+    
+   public function Compare($Astudent, $Bstudent)
+   {
+    $studentA =  array();
+    $studentB = array();
+	$assigmentcount=count($Astudent);
+
+   for($c=0;$c<$assigmentcount;$c++)
+   {
+    if($Astudent[$c]==$Bstudent[$c])
     {
-        echo 'Yey! The constructor works...' . PHP_EOL;
+     $studentA[$c]=0;
+     $studentB[$c]=0;
+     continue;
     }
+    if($Astudent[$c]>$Bstudent[$c])
+    {
+     $studentA[$c]=1;
+     $studentB[$c]=0;
+     continue;
+    }
+    if($Astudent[$c]<$Bstudent[$c])
+    {
+     $studentA[$c]=0;
+     $studentB[$c]=1;
+     continue;
+    }
+   }
+   for ($i=0;$i<$assigmentcount;$i++)
+    {
+     echo $studentA[$i]." ";
+     
+    }
+     echo " = ".array_sum($studentA).PHP_EOL;
+   for ($i=0;$i<$assigmentcount;$i++)
+    {
+     echo $studentB[$i]." ";
+
+    }
+    echo " = ".array_sum($studentB).PHP_EOL;
+   }
 }
